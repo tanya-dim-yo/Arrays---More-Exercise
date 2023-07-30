1ba71b7ced1f54ec73fdef704be416da04a07808
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-07-30 05:08:58 +0300

    Add files via upload

---
+++ 02. Pascal Triangle/Program.cs
@@ -0,0 +1,20 @@
+﻿int n = int.Parse(Console.ReadLine());
+ulong[] previousArray = new ulong[n];
+
+for (int i = 1; i <= n; i++)
+{
+    ulong[] array = new ulong[i];
+    array[0] = 1;
+    array[array.Length - 1] = 1;
+
+    if (array.Length > 2)
+    {
+        for (int j = 1; j <= array.Length - 2; j++)
+        {
+            array[j] = previousArray[j - 1] + previousArray[j];
+        }
+    }
+
+    previousArray = array;
+    Console.WriteLine(string.Join(" ", array));
+}
