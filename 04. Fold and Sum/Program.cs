fe2ded9701ddcd2e747d953da43aaa1d00589b53
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-08-01 05:12:25 +0300

    Add files via upload

---
+++ 04. Fold and Sum/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] initialSequence = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int[] firstRowLeft = new int[initialSequence.Length / 4];
+int[] firstRowRight = new int[initialSequence.Length / 4];
+
+for (int i = 0; i < firstRowLeft.Length; i++)
+{
+    firstRowLeft[i] = initialSequence[firstRowLeft.Length - 1 - i];
+}
+
+for (int i = 0; i < firstRowRight.Length; i++)
+{
+    firstRowRight[i] = initialSequence[initialSequence.Length - 1 - i];
+}
+
+int[] firstRow = firstRowLeft.Concat(firstRowRight).ToArray();
+int[] secondRow = new int[initialSequence.Length / 2];
+
+for (int i = 0; i < secondRow.Length; i++)
+{
+    secondRow[i] = initialSequence[firstRowLeft.Length + i];
+}
+
+int[] finalArray = new int[secondRow.Length];
+
+for (int i = 0; i < finalArray.Length; i++)
+{
+    finalArray[i] = firstRow[i] + secondRow[i];
+}
+
+Console.WriteLine(string.Join(" ", finalArray));
