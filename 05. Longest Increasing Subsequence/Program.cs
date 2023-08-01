ae573ec701869ca17af0898bdc7622c64a6109fb
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-08-01 05:13:15 +0300

    Add files via upload

---
+++ 05. Longest Increasing Subsequence/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int[] len = new int[nums.Length];
+
+for (int p = 0; p < nums.Length; p++)
+{
+    int left = 0;
+
+    for (int i = 0; i < p; i++)
+    {
+        if (nums[i + 1] > nums[i])
+        {
+            left = i + 1;
+        }
+    }
+
+    len[p] = 1 + len[left];
+
+    len[p] = 1;
+}
